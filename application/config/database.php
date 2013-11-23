cdf084d6a5946f5677fd3fc651d0facfbc950382
gerbenzomp <mail@zomp.nl>
2013-11-23 23:15:55 +0100

    Removed path

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'dashbird2';
-$db['default']['password'] = 'spaarlamp23';
-$db['default']['database'] = 'admin_dashbird2';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
