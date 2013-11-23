53eb75c3b28f62c4534e6ab6af4b649a4f8b7e8f
gerbenzomp <mail@zomp.nl>
2013-11-23 23:26:04 +0100

    Fixed the navbar

--- application/plugins/icon-toolbar/navbar.php
+++ application/plugins/icon-toolbar/navbar.php
@@ -5,15 +5,15 @@ $CI=&get_instance();
 <div id="navbar">
 
 <div id="navbar-wrapper">
-<a href="<?=base_url();?>" class="nav-button<?php if($CI->uri->segment(1)!='dashboard'){ echo " current"; } ?>">Website</a><a href="<?=base_url();?>dashboard" class="nav-button<?php if($CI->uri->segment(1)=='dashboard'){ echo " current"; } ?>">Instellingen</a>
+<a href="<?=base_url();?>" class="nav-button<?php if($CI->uri->segment(1)=='' || $CI->uri->segment(1)=='page'){ echo " current"; } ?>">Website</a><a href="<?=base_url();?>dashboard" class="nav-button<?php if($CI->uri->segment(1) && $CI->uri->segment(1)!='page'){ echo " current"; } ?>">Instellingen</a>
 
 <a class="nav-button" id="logout-button" style="float: right; width: 105px;" id="userinfo" href="<?=base_url();?>users/logout"><i style="margin-right: 7px; position: relative; top: -1px;"><img src="<?=base_url();?>application/plugins/icon-toolbar/navbar/img/user.png" /></i> <span class="small-hide"><?=ucwords($CI->session->userdata('username'));?></span></a>
 
 
-<?php if($CI->uri->segment(1)!='dashboard'){ ?>
+<?php if($CI->uri->segment(1)=='' || $CI->uri->segment(1)=='page'){ ?>
 <a class="nav-button add-button" style="float: right;" id="col-main" data-page="<?=$CI->session->userdata('curpage');?>"><i style="margin-right: 7px; position: relative; top: -2px;"><img src="<?=base_url();?>application/plugins/icon-toolbar/navbar/img/plus.png" /></i><span class="small-hide"> Nieuw artikel schrijven</span></a>
 <?php }else{ ?>
-<a href="#!/add/posts" class="nav-button" style="float: right;" onclick="$('.current-item').removeClass('current-item');$('#posts').addClass('current-item');"><i style="margin-right: 7px;  position: relative; top: -2px;"><img src="<?=base_url();?>application/plugins/icon-toolbar/navbar/img/plus.png" /></i> <span class="small-hide">Nieuw artikel schrijven</span></a>
+<a href="<?=base_url();?>posts/edit" class="nav-button" style="float: right;" onclick="$('.current-item').removeClass('current-item');$('#posts').addClass('current-item');"><i style="margin-right: 7px;  position: relative; top: -2px;"><img src="<?=base_url();?>application/plugins/icon-toolbar/navbar/img/plus.png" /></i> <span class="small-hide">Nieuw artikel schrijven</span></a>
 <?php } ?>
 
 </div>
