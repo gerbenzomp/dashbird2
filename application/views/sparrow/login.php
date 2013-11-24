24bb7b333fdc542a965c8cd2cfc59d84cae91805
gerbenzomp <mail@zomp.nl>
2013-11-24 15:28:46 +0100

    Fixed the login screen

--- application/views/sparrow/login.php
+++ application/views/sparrow/login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -13,7 +13,7 @@
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
 <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
-<link rel="stylesheet" href="<?=base_url();?>application/views/sparrow/style2.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="<?=base_url();?>application/views/sparrow/style.css" type="text/css" media="screen" />
 
 
 </head>
